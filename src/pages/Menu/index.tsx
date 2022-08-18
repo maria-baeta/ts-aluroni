225a16dc6f8b06e0e2bcf0d447ac81b8f17013a6
maria_baeta <maria.e.martins@somoseducacao.com.br>
2022-08-18 17:45:56 -0300

    feat: Build de arquivos estaticos como SVG

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,9 +1,12 @@
 import styles from "./Menu.module.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 export const Menu = () => {
   return (
     <main>
-      <nav className="menu"></nav>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
     </main>
   );
 };
