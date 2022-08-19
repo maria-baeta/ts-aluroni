5e5276d501f6bb9ccc39701dfe1b1adcd1b918c2
maria_baeta <maria.e.martins@somoseducacao.com.br>
2022-08-19 11:14:29 -0300

    feat: Variavies:
    Criando variaveis para o Css

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -7,6 +7,9 @@ export const Menu = () => {
       <nav className={styles.menu}>
         <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da Massa</div>
+      </header>
     </main>
   );
 };
